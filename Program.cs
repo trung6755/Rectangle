dcd8653968f07d9f9d5510fb7c1c95e49d9a410e
trung6755 <trungggrr@gmail.com>
2024-03-05 15:26:52 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Enter width: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            double height = double.Parse(Console.ReadLine());
+
+            rectangle rec1 = new rectangle(width,height);
+
+            Console.WriteLine("The area of the rectangle is " + rec1.getArea());
+            Console.WriteLine("The perimeter of the rectangle is " + rec1.getPerimeter());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ rectangle.cs
@@ -0,0 +1,29 @@
+﻿namespace Rectangle
+{
+    public class rectangle
+    {
+        double width;
+        double height;
+
+        public rectangle()
+        {
+
+        }
+
+        public rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double getArea()
+        {
+            return this.width * this.height;
+        }
+
+        public double getPerimeter()
+        {
+            return (this.width + this.height) * 2;
+        }
+    }
+}
